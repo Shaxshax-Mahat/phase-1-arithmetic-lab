69c94150e0e0bd931f0737b641023e6c03e84b96
Shaxshax-Mahat <abdulrahmanmahat0@gmail.com>
2025-06-15 19:17:13 +0300

    Modifies index.html

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let result = multiply;
+console.log(result);
+
+//random
+let random = Math.floor(Math.random() * 100) + 1;
+
+//mod
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4;
+console.log(mod);
+
+//max
+let max = Math.max(2,4,6,12,14,15,20);
